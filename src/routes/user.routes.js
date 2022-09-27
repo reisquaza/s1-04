40a253ca9b3fee2fbfd4f3878c8df544314bff05
Reisquaza <100309835+Reisquaza@users.noreply.github.com>
2022-09-27 13:14:45 -0300

    Feat: authorization token middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,6 +5,7 @@ import deleteUserController from "../controllers/deleteUser.controller";
 import listUsersController from "../controllers/listUsers.controller";
 import updateUserController from "../controllers/updateUser.controller";
 import userLoginController from "../controllers/userLogin.controller";
+import verifyAuthTokenMiddleware from "../middlewares/verifyAuthToken.middleware";
 import verifyEmailAvailabilityMiddleware from "../middlewares/verifyEmailAvailability.middleware";
 
 const router = Router();
@@ -13,9 +14,9 @@ router.post("/", verifyEmailAvailabilityMiddleware, createUserController);
 
 router.get("/", listUsersController);
 
-router.put("/:id", updateUserController);
+router.put("/:id", verifyAuthTokenMiddleware, updateUserController);
 
-router.delete("/:id", deleteUserController);
+router.delete("/:id", verifyAuthTokenMiddleware, deleteUserController);
 
 router.post("/login", userLoginController);
 
